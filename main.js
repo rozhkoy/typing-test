b794b36d8af93b6516e6f8c15dec6ebf75c592e2
zolir18 <rc.drift18@gmail.com>
2021-05-06 00:33:08 +0200

    modified js

--- main.js
+++ main.js
@@ -1,38 +1,54 @@
 const inputText = document.getElementById('inputchar');
-let checkWord = 'word';
+//add enter field 
+
+let checkWord = ['word', 'time', 'match'];
+// check word
 let words;
-document.addEventListener('keyup', function() {
+let checkIndex = 0
+let indexWord = 0;
+document.addEventListener('keyup', returEnteredWord, false);
+
+
+function returEnteredWord() {
     words = inputText.value;
+    //return value input
     arrayWords = words.split('');
+    //divide up value on array
     arrayWordsSize = arrayWords.length - 1;
-
-
+    //size array
     if (arrayWords[arrayWordsSize] == " ") {
-        console.log('yes');
-        document.getElementById('inputchar').value = "";
-        arrayCheckWord = checkWord.split('');
-        let checkIndex = 0;
-        if (arrayCheckWord.length == arrayWordsSize) {
-            console.log('ooohhh');
-            for (let i = 0;)
+        compareWord();
+    }
+    return arrayWords, arrayWordsSize;
+}
 
 
-        } else {
-            console.log('nooo');
-        }
-
-
-        checkIndex++;
-
-
-
-
+function preparationWord() {
+    if (checkWord[indexWord] == undefined) {
+        console.log('ended test');
+    } else {
+        arrayCheckWord = checkWord[indexWord].split('');
     }
 
+}
 
-
-
-    // console.log(words);
-    // console.log(arrayWords);
-    // console.log(arrayWordsSize)
-})
+function compareWord() {
+    preparationWord();
+    indexWord++;
+    document.getElementById('inputchar').value = "";
+    if (arrayCheckWord.length == arrayWordsSize) {
+        checkIndex = 0;
+        for (let i = 0; i <= arrayCheckWord.length; i++) {
+            if (arrayCheckWord[checkIndex] == arrayWords[checkIndex]) {
+                checkIndex++;
+            }
+        }
+        if (checkIndex == arrayCheckWord.length) {
+            console.log('ok');
+        } else {
+            console.log('no ok');
+        }
+    } else {
+        console.log('no match');
+    }
+}
